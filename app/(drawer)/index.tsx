14510f1f58a2b57713d4df903c85bdc2052c5d1d
veerkam1797 <your-email@domain.com>
2025-12-25 22:30:34 +0530

    Created Braille, Sign & Settings Screns.

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,14 +1,73 @@
-import { SignOutButton } from '@/components/buttons/SignOutButton';
+import CommonIconButton from '@/components/buttons/CommonIconButton';
+import CommonTextInput from '@/components/CommonTextInput';
+import BrailleSection from '@/components/sections/BrailleSection';
+import SignSection from '@/components/sections/SignSection';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native';
 
 export default function HomeScreen() {
+  const [transcribedText, setTranscribedText] = React.useState<string>('');
+  const [transcriptionMode, setTranscriptionMode] =
+    React.useState<any>('braille');
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <SignOutButton />
-    </View>
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={80}
+      enabled>
+      <View style={{flex: 1}}>
+        {!transcriptionMode ? <SignSection /> : <BrailleSection />}
+      </View>
+      <View style={styles.transcriptContainer}>
+        {/* Button to change insert files */}
+        <CommonIconButton
+          mode="contained"
+          icon="attachment"
+          iconSize={24}
+          iconColor=""
+          onPress={() => {}}
+          extraStyle={{margin: 0}}
+          contentStyle={{}}
+        />
+        {/* Text box to show transcription and make corrections  */}
+        <CommonTextInput
+          label="Your text will show here"
+          placeholder="Wanna change something?"
+          autoCapitalize=""
+          value={transcribedText}
+          onChangeText={text => setTranscribedText(text)}
+          secureText={false}
+          rightIcon={'send'}
+          onPress={() => {}}
+          extraStyle={{flex: 1}}
+          outlineStyle={{borderRadius: 48 / 2}}
+          dense={true}
+        />
+        {/* Button to change transcription mode */}
+        <CommonIconButton
+          mode="contained"
+          icon={!transcriptionMode ? 'sign-language' : 'braille'}
+          iconSize={24}
+          iconColor=""
+          onPress={() => {
+            setTranscriptionMode(!transcriptionMode);
+          }}
+          extraStyle={{margin: 0}}
+          contentStyle={{}}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  transcriptContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    paddingTop: 8,
+    paddingBottom: 12.8,
+    gap: 8,
+    width: '100%',
+  },
+});
--- app/(drawer)/settings.tsx
+++ app/(drawer)/settings.tsx
@@ -1,11 +1,20 @@
+import { SignOutButton } from '@/components/buttons/SignOutButton';
+import SettingsSection from '@/components/sections/SettingsSection';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
 export default function SettingsScreen() {
   return (
-    <View>
-      <Text>SettingsScreen</Text>
-    </View>
+    <ScrollView
+      style={{flex: 1}}
+      contentContainerStyle={{
+        flexGrow: 1,
+      }}>
+      <SettingsSection />
+      <View style={{padding: 16}}>
+        <SignOutButton />
+      </View>
+    </ScrollView>
   );
 }
 
